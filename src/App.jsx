1afe39a6a3a5e15aa133ec5841315f0269b8fddd
kliuha.illia <kliuha.illia@gmail.com>
2025-10-24 15:14:10 +0300

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { Home, About, Contacts } from "./pages";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 const App = () => {
   return (
     <main className="bg-slate-300/20 h-[100vh]">
